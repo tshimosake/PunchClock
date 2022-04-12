dbca89a510e133ef335fa9a558995f961bd032af
tshimosake <t.shimosake@gmail.com>
2022-04-12 20:07:32 +0900

    first commiting

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  get 'posts/index' => "posts#index"
+  get "posts/new" => "posts#new"
+  get "posts/:id" => "posts#show"
+  get "posts/:id/edit" => "posts#edit"
+  get "/" => "home#top"
+  get 'top' => "home#top"
+  get "about" => "home#about"
+  post "posts/create" => "posts#create"
+  post "posts/:id/update" => "posts#update"
+  post "posts/:id/destroy" => "posts#destroy"
+  
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
