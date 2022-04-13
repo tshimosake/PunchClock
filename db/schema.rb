5e200976a3bfceff607ad10895785e19b8a30fbb
tshimosake <102135053+tshimosake@users.noreply.github.com>
2022-04-13 20:57:39 +0900

    ログイン機能を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_13_024125) do
+ActiveRecord::Schema.define(version: 2022_04_13_111750) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2022_04_13_024125) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password"
   end
 
 end
