3398211e7aa7c00aa3cef9b5efe5f48b5c5d4a56
tshimosake <102135053+tshimosake@users.noreply.github.com>
2022-04-13 21:41:28 +0900

    投稿一覧で時刻を見られるようにした

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
     validates :content, {presence: true, length: {maximum: 140}}
+    validates :time_in, {presence: true}
+    validates :time_out, {presence: true}
 end
