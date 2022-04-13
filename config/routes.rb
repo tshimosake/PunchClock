f5c9d8b0555f51c51206cabfd7729b4ec9f4f192
tshimosake <102135053+tshimosake@users.noreply.github.com>
2022-04-13 19:34:26 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
   get 'top' => "home#top"
   get "about" => "home#about"
 
-resources :posts
+resources :home
 root 'home#top' 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
